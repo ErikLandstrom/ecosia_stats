a7d92c5a5b87679a7353fb0daa990fc144b2be61
Erik Ländström <erik.landstrom@hotmail.se>
2019-04-09 17:33:26 +0200

    Download ecosia report files

---
+++ download_ecosia_reports.R
@@ -0,0 +1,7 @@
+download_ecosia_reports <- function(urls, dest_file = "images/", year, month) {
+  for(i in 1:length(urls)) {
+    download.file(urls[i],
+                  destfile = paste("images/2018/", str_extract(month[i], "[a-z]{3}"), "_", year, ".jpg", sep = ""),
+                  mode = "wb")
+  }
+}
