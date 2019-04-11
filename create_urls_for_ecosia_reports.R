1d45e8e259027425a11cd619e46161828ff1ecc4
Erik Ländström <erik.landstrom@hotmail.se>
2019-04-11 16:35:04 +0200

    updated syntax

--- create_urls_for_ecosia_reports.R
+++ create_urls_for_ecosia_reports.R
@@ -1,7 +1,7 @@
 create_urls_for_ecosia_reports <- function(url, month, year) {
   urls <- as_vector(1:12)
   for(i in 1:length(urls)) {
-    urls[i] <- paste(url, month[i], "-", year, "-EN.jpg", sep = "")
+    urls[i] <- paste(url, "-", month[i], "-", year, "-EN.jpg", sep = "")
   }
   
   return(urls)
